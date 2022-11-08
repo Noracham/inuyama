41052bb72ae7bba74646aa6b70f4c71a934681ae
Nora <nsorym04@gmail.com>
2022-11-08 22:48:53 +0900

    main,news仮完成

--- js/index.js
+++ js/index.js
@@ -2,4 +2,23 @@ $(function(){
     $(".openbtn").click(function(){
         $(this).toggleClass("active");
     })
+
+    // vegas slide
+    $("#slider").vegas({
+        overlay:true,
+        transition:"blur",
+        transitionDuration:1000,
+        delay:4000,
+        animationDuration:10000,
+        animation:"random",
+        slides:[
+            {src:"./img/shop1_visu1.jpeg"},
+            {src:"./img/shop1_visu2.jpeg"},
+            {src:"./img/shop3_visu1.jpeg"},
+            {src:"./img/shop3_visu2.jpeg"}
+            ],
+        timer:true,
+        overlay: './img/09.png'
+    });
+
 })
