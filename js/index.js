7d38a35f7b03b6097e5416872264967fc78be1cf
Nora <nsorym04@gmail.com>
2022-11-10 06:23:05 +0900

    cardにアニメーションを追加

--- js/index.js
+++ js/index.js
@@ -21,4 +21,28 @@ $(function(){
         overlay: './img/09.png'
     });
 
+    //scroll event
+    let windowSize = $(window).height();
+    console.log(windowSize);
+
+    // アニメーションの配列
+    sideFadeInAnimation = [
+        "#card1",
+        "#card2",
+        "#card3"
+    ]
+
+    $(window).scroll(function () { 
+        let scrollPosition = $(this).scrollTop();
+        // console.log(scrollPosition);
+
+            var offset = $("#card1").offset();
+            var top = offset.top;
+            
+            if(scrollPosition > top - windowSize + 300){
+                $("#card1 img:nth-of-type(1)").addClass("left_In")
+                $("#card1 img:nth-of-type(3)").addClass("In")
+            }
+    });
+
 })
