d5a9cb09d792b5743054fa81fc12ecf34579d81c
Nora <nsorym04@gmail.com>
2022-11-10 15:08:10 +0900

    shops途中

--- js/index.js
+++ js/index.js
@@ -39,10 +39,38 @@ $(function(){
             var offset = $("#card1").offset();
             var top = offset.top;
             
-            if(scrollPosition > top - windowSize + 300){
-                $("#card1 img:nth-of-type(1)").addClass("left_In")
-                $("#card1 img:nth-of-type(3)").addClass("In")
+            if(scrollPosition > top - windowSize + 100){
+                $("#card1 img:nth-of-type(1)").addClass("left_In");
+                $("#card1 img:nth-of-type(2)").addClass("center_In");
+                $("#card1 img:nth-of-type(3)").addClass("r_In");
             }
     });
+    $(window).scroll(function () { 
+        let scrollPosition = $(this).scrollTop();
+        // console.log(scrollPosition);
+
+            var offset = $("#card2").offset();
+            var top = offset.top;
+            
+            if(scrollPosition > top - windowSize + 100){
+                $("#card2 img:nth-of-type(1)").addClass("left_In");
+                $("#card2 img:nth-of-type(2)").addClass("center_In");
+                $("#card2 img:nth-of-type(3)").addClass("r_In");
+            }
+    });
+    $(window).scroll(function () { 
+        let scrollPosition = $(this).scrollTop();
+        // console.log(scrollPosition);
+
+            var offset = $("#card3").offset();
+            var top = offset.top;
+            
+            if(scrollPosition > top - windowSize + 100){
+                $("#card3 img:nth-of-type(1)").addClass("left_In");
+                $("#card3 img:nth-of-type(2)").addClass("center_In");
+                $("#card3 img:nth-of-type(3)").addClass("r_In");
+            }
+    });
+
 
 })
